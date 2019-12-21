870583afcb689c1f01b3ac3acba24ac650c148af
Ram Sethuraman <ramsethuraman123@gmail.com>
2019-12-20 23:32:12 -0600

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,26 +3,28 @@ class myStack:
      def __init__(self):
          self.myStack=[]
          
-     def isEmpty(self):
+     def isEmpty(self): #Time complexity is O(1)
          if len(self.myStack)==0:
              print("Stack is empty")
-     def push(self, item):
+             
+        
+     def push(self, item): #Time complexity is O(1)
          self.myStack.append(item)
          
-     def pop(self):
+     def pop(self): #Time complexity is O(1)
          if len(self.myStack)==0:
              print("stack is empty. Unable to pop")
              return 0
          else:
              return self.myStack.pop()
 
-     def peek(self):
+     def peek(self): #Time complexity is O(1)
          return(self.myStack[-1])
          
-     def size(self):
+     def size(self): #Time complexity is O(1)
          return(len(self.myStack))
          
-     def show(self):
+     def show(self): #Time complexity is O(N)
          return(self.myStack)
          
 
